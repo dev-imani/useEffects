886a9744e00bc123b499db97fdec351a8bcaff05
dev-imani <michaelimani396@gmail.com>
2025-04-18 14:27:18 +0300

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       <h1>I am a software Engineer</h1>
       <Timer />
       <Timer2/>
+      <Counter/>
     </div>
   );
 }
@@ -35,6 +36,25 @@ function Timer2() {
   }, []); // <- add empty brackets here
 
   return <h1>I've rendered {count} times!</h1>;
+}
+
+
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [calculation, setCalculation] = useState(0);
+
+  useEffect(() => {
+    setCalculation(() => count * 2);
+  }, [count]); // <- add the count variable here
+
+  return (
+    <>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount((c) => c + 1)}>+</button>
+      <p>Calculation: {calculation}</p>
+    </>
+  );
 }
 
 
